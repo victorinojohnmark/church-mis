e1e36ea1d986bc3787817ae8a3c8a853940bdf6b
victorinojohnmark <victorinojohnmark@gmail.com>
2023-05-23 16:53:48 +0800

    bootstrap integrated, header component added

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,10 +1,13 @@
-import 'bootstrap';
-
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
+
+import * as Popper from '@popperjs/core'
+window.Popper = Popper
+
+import 'bootstrap';
 
 import axios from 'axios';
 window.axios = axios;
