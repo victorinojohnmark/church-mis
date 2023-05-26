7a51f855b3e63cda81a60a7190aaeb9e8a10ac57
John Mark Victorino <victorinojohnmark@gmail.com>
2023-05-26 21:56:49 +0800

    update welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -53,7 +53,7 @@
 
 <div id="featureWrapper">
   <div class="container">
-    <div class="row gx-5 row-cols-1 row-cols-md-2 m-0">
+    <div class="row gx-5 row-cols-1 row-cols-md-3 m-0">
         <div class="col mb-5 h-100 py-3">
             <div class="feature bg-secondary text-white rounded-3 mb-3">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-size">
@@ -74,6 +74,17 @@
             <p class="mb-3">Paragraph of text beneath the heading to explain the heading. Here is just a bit more text.</p>
             <a href="#">Submit a request</a>
         </div>
+
+        <div class="col mb-5 h-100 py-3">
+          <div class="feature bg-secondary text-white rounded-3 mb-3">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-size">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M21 11.25v8.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 109.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1114.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
+            </svg>                                 
+          </div>
+          <h2 class="h5 text-success">Donate</h2>
+          <p class="mb-3">Paragraph of text beneath the heading to explain the heading. Here is just a bit more text.</p>
+          <a href="#">Give</a>
+      </div>
     </div>
   </div>
 </div>
