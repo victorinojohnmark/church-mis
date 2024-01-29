6a71e2d42bd153653d92092163ed2f1c42bc75fd
joshua.palma.dev2020 <joshua.palma.dev2020@gmail.com>
2024-01-29 14:59:22 +0800

    commit fix UI

--- resources/views/user/documentrequest/menu.blade.php
+++ resources/views/user/documentrequest/menu.blade.php
@@ -1,8 +1,8 @@
 <div class="py-3">
     <a href="{{ route('client-documentrequestbaptismlist') }}" class="btn btn-success">Baptism</a>
     <a href="{{ route('client-documentrequestconfirmationlist') }}" class="btn btn-success">Confirmation</a>
-    <a href="{{ route('client-documentrequestcommunionlist') }}" class="btn btn-success">First Communion</a>
+    {{-- <a href="{{ route('client-documentrequestcommunionlist') }}" class="btn btn-success">First Communion</a> --}}
     {{-- <a href="{{ route('client-documentrequestmatrimonylist') }}" class="btn btn-success">Wedding</a> --}}
     {{-- <a href="{{ route('client-documentrequestblessinglist') }}" class="btn btn-success">Blessings</a> --}}
-    <a href="{{ route('client-documentrequestdeathlist') }}" class="btn btn-success">Death</a>
+    {{-- <a href="{{ route('client-documentrequestdeathlist') }}" class="btn btn-success">Death</a> --}}
 </div>
--- resources/views/user/reservations/reservation-menu.blade.php
+++ resources/views/user/reservations/reservation-menu.blade.php
@@ -1,8 +1,8 @@
 <div class="py-3">
     {{-- options here --}}
     <a href="{{ route('clientbaptism') }}" class="btn btn-primary rounded-pill mb-1 {{ request()->routeIs('clientbaptism') ? 'active' : '' }}">Baptism</a>
-    <a href="{{ route('clientcommunion') }}" class="btn btn-primary rounded-pill mb-1 {{ request()->routeIs('clientcommunion') ? 'active' : '' }}">First Holy Communion</a>
-    <a href="{{ route('clientconfirmation') }}" class="btn btn-primary rounded-pill mb-1 {{ request()->routeIs('clientconfirmation') ? 'active' : '' }}">Confirmation</a>
+    {{-- <a href="{{ route('clientcommunion') }}" class="btn btn-primary rounded-pill mb-1 {{ request()->routeIs('clientcommunion') ? 'active' : '' }}">First Holy Communion</a> --}}
+    {{-- <a href="{{ route('clientconfirmation') }}" class="btn btn-primary rounded-pill mb-1 {{ request()->routeIs('clientconfirmation') ? 'active' : '' }}">Confirmation</a> --}}
     <a href="{{ route('clientmatrimony') }}" class="btn btn-primary rounded-pill mb-1 {{ request()->routeIs('clientmatrimony') ? 'active' : '' }}">Wedding</a>
     <a href="{{ route('clientblessing') }}" class="btn btn-primary rounded-pill mb-1 {{ request()->routeIs('clientblessing') ? 'active' : '' }}">Blessings</a>
     <a href="{{ route('clientfuneral') }}" class="btn btn-primary rounded-pill mb-1 {{ request()->routeIs('clientfuneral') ? 'active' : '' }}">Funeral</a>
