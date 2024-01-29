dd3c6fc4988e7719ded850e9b89a6148317411bd
joshua.palma.dev2020 <joshua.palma.dev2020@gmail.com>
2024-01-29 13:18:54 +0800

    commit add catechist user level

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('is_admin')->default(false);
+            $table->string('role')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
 use App\Models\User;
+use Carbon\Carbon;
 
 class UserSeeder extends Seeder
 {
@@ -15,14 +16,31 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
-            'name' => 'Admin',
-            'birth_date' => '2023-05-23',
-            'address' => 'Cavite',
-            'contact_number' => '091234567890',
-            'email' => 'saintgregoryparish.indang@gmail.com',
-            'password' => Hash::make('password'),
-            'is_admin' => true
+        User::insert([
+            [
+                'name' => 'Admin',
+                'birth_date' => '2023-05-23',
+                'address' => 'Cavite',
+                'contact_number' => '091234567890',
+                'email' => 'saintgregoryparish.indang@gmail.com',
+                'password' => Hash::make('password'),
+                'is_admin' => true,
+                'role' => 'Admin',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'Catechist',
+                'birth_date' => '2023-05-23',
+                'address' => 'Cavite',
+                'contact_number' => '091234567890',
+                'email' => 'saintgregoryparish.indang.cathechist@gmail.com',
+                'password' => Hash::make('password'),
+                'is_admin' => false,
+                'role' => 'Catechist',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]
         ]);
     }
 }
